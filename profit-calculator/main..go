28c30316415c3c7775d8c674d2d103428a4c5886
Jayson Gepayo <jaygepayo.1994@gmail.com>
2026-05-04 14:51:23 +0800

    custom functions

--- profit-calculator/main..go
+++ profit-calculator/main..go
@@ -5,24 +5,29 @@ import (
 )
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	ebt := revenue - expenses
-	profit := ebt * (1 - taxRate/100)
-	ratio := ebt / profit
+	ebt, profit, ratio := calculate(revenue, expenses, taxRate)
 
 	fmt.Println("Earnings Before Tax:", ebt)
 	fmt.Println("Profit:", profit)
 	fmt.Println("Ratio:", ratio)
+}
+
+func getUserInput(text string) float64 {
+	var userInput float64
+	fmt.Print(text)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculate(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	return ebt, profit, ratio
 }
