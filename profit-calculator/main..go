dbfe0a94701c8576d515882f216eaa58d18a05c7
Jayson Gepayo <jaygepayo.1994@gmail.com>
2026-05-07 16:15:02 +0800

    add looping + add writefile

--- profit-calculator/main..go
+++ profit-calculator/main..go
@@ -2,19 +2,70 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
+
+const revenueFile = "revenue.txt"
+const expensesFile = "expenses.txt"
+const taxRateFile = "taxRate.txt"
+const ratioFile = "ratio.txt"
+
+func writeToFile(revenue, expenses, tax, ratio float64) {
+
+	revenueText := fmt.Sprint(revenue)
+	os.WriteFile(revenueFile, []byte(revenueText), 0644)
+
+	expensesText := fmt.Sprint(expenses)
+	os.WriteFile(expensesFile, []byte(expensesText), 0644)
+
+	taxRate := fmt.Sprint(tax)
+	os.WriteFile(taxRateFile, []byte(taxRate), 0644)
+
+	ratioText := fmt.Sprint(ratio)
+	os.WriteFile(ratioFile, []byte(ratioText), 0644)
+}
 
 func main() {
 
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	var choice int
 
-	ebt, profit, ratio := calculate(revenue, expenses, taxRate)
+	fmt.Println("WELCOME TO PROFIT CALCULATOR")
 
-	fmt.Printf("Earnings Before Tax: %0.2f\n", ebt)
-	fmt.Printf("Profit: %0.2f\n", profit)
-	fmt.Printf("Ratio: %0.2f", ratio)
+	for {
+		fmt.Println("What do you want to do")
+		fmt.Println("1. Compute for Profit")
+		fmt.Println("2. EXIT")
+		fmt.Scan(&choice)
+
+		if choice == 1 {
+			revenue := getUserInput("Revenue: ")
+			if revenue <= 0 {
+				fmt.Println("Please enter valid revenue")
+				continue
+			}
+			expenses := getUserInput("Expenses: ")
+			if expenses <= 0 {
+				fmt.Println("Please valid expenses")
+				continue
+			}
+			taxRate := getUserInput("Tax Rate: ")
+			if taxRate <= 0 {
+				fmt.Println("Please valid tax rate")
+				continue
+			}
+
+			ebt, profit, ratio := calculate(revenue, expenses, taxRate)
+
+			fmt.Printf("Earnings Before Tax: %0.2f\n", ebt)
+			fmt.Printf("Profit: %0.2f\n", profit)
+			fmt.Printf("Ratio: %0.2f \n\n", ratio)
+			writeToFile(ebt, profit, taxRate, ratio)
+		} else {
+			fmt.Println("Goodbye")
+			fmt.Println("Thank you")
+			return
+		}
+	}
 }
 
 func getUserInput(text string) float64 {
